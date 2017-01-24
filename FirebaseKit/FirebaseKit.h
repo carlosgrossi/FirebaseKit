ba4393cc07a98f3012991843138c1808b82b03f4
Carlos Grossi <carlos.grossi@gmail.com>
2017-01-24 20:06:25 -0200

    Initial Commit

---
+++ FirebaseKit/FirebaseKit.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseKit.h
+//  FirebaseKit
+//
+//  Created by Carlos Grossi on 24/01/17.
+//  Copyright © 2017 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseKit.
+FOUNDATION_EXPORT double FirebaseKitVersionNumber;
+
+//! Project version string for FirebaseKit.
+FOUNDATION_EXPORT const unsigned char FirebaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseKit/PublicHeader.h>
+
+
